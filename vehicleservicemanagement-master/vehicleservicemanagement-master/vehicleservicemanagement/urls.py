07b419d1f8db0c68144e428c903b77195293603b
Theertha <theertha1808@gmail.com>
2024-01-26 20:31:30 +0100

    Cleaned code

--- vehicleservicemanagement-master/vehicleservicemanagement-master/vehicleservicemanagement/urls.py
+++ vehicleservicemanagement-master/vehicleservicemanagement-master/vehicleservicemanagement/urls.py
@@ -15,44 +15,9 @@ urlpatterns = [
 
     path('',views.home_view,name=''),
 
-    path('adminclick', views.adminclick_view),
     path('customerclick', views.customerclick_view),
-
     path('customersignup', views.customer_signup_view,name='customersignup'),
-
     path('customerlogin', LoginView.as_view(template_name='vehicle/customerlogin.html'),name='customerlogin'),
-    path('adminlogin', LoginView.as_view(template_name='vehicle/adminlogin.html'),name='adminlogin'),
-
-
-
-    path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
-
-    path('admin-customer', views.admin_customer_view,name='admin-customer'),
-    path('admin-view-customer',views.admin_view_customer_view,name='admin-view-customer'),
-    path('delete-customer/<int:pk>', views.delete_customer_view,name='delete-customer'),
-    path('update-customer/<int:pk>', views.update_customer_view,name='update-customer'),
-    path('admin-add-customer', views.admin_add_customer_view,name='admin-add-customer'),
-    path('admin-view-customer-enquiry', views.admin_view_customer_enquiry_view,name='admin-view-customer-enquiry'),
-    path('admin-view-customer-invoice', views.admin_view_customer_invoice_view,name='admin-view-customer-invoice'),
-
-
-    path('admin-request', views.admin_request_view,name='admin-request'),
-    path('admin-view-request',views.admin_view_request_view,name='admin-view-request'),
-    path('change-status/<int:pk>', views.change_status_view,name='change-status'),
-    path('admin-delete-request/<int:pk>', views.admin_delete_request_view,name='admin-delete-request'),
-    path('admin-add-request',views.admin_add_request_view,name='admin-add-request'),
-    path('admin-approve-request',views.admin_approve_request_view,name='admin-approve-request'),
-    path('approve-request/<int:pk>', views.approve_request_view,name='approve-request'),
-
-    path('admin-view-service-cost',views.admin_view_service_cost_view,name='admin-view-service-cost'),
-    path('update-cost/<int:pk>', views.update_cost_view,name='update-cost'),
-
-    path('admin-mechanic-attendance', views.admin_mechanic_attendance_view,name='admin-mechanic-attendance'),
-    path('admin-take-attendance', views.admin_take_attendance_view,name='admin-take-attendance'),
-    path('admin-view-attendance', views.admin_view_attendance_view,name='admin-view-attendance'),
-    path('admin-feedback', views.admin_feedback_view,name='admin-feedback'),
-
-    path('admin-report', views.admin_report_view,name='admin-report'),
 
     path('customer-dashboard', views.customer_dashboard_view,name='customer-dashboard'),
     path('customer-request', views.customer_request_view,name='customer-request'),
@@ -60,19 +25,15 @@ urlpatterns = [
 
     path('customer-profile', views.customer_profile_view,name='customer-profile'),
     path('edit-customer-profile', views.edit_customer_profile_view,name='edit-customer-profile'),
-    path('customer-feedback', views.customer_feedback_view,name='customer-feedback'),
-    path('customer-invoice', views.customer_invoice_view,name='customer-invoice'),
+
     path('customer-view-request/',views.customer_view_request_view,name='customer-view-request'),
-    path('customer-edit-request/<int:pk>', views.customer_delete_request_view,name='customer-edit-request'),
-    path('customer-delete-request/<int:pk>', views.customer_delete_request_view,name='customer-delete-request'),
+
     path('customer-view-approved-request',views.customer_view_approved_request_view,name='customer-view-approved-request'),
     path('customer-view-approved-request-invoice',views.customer_view_approved_request_invoice_view,name='customer-view-approved-request-invoice'),
-    #path('delete-enquiry/<int:service_id>/', views.delete_enquiry, name='delete_enquiry'),
     path('api/v1/serviceManagement/<int:service_id>', views.delete_service, name='delete_service'),
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
     path('logout', LogoutView.as_view(template_name='vehicle/index.html'),name='logout'),
 
     path('aboutus', views.aboutus_view),
-    path('contactus', views.contactus_view),
 
 ]
